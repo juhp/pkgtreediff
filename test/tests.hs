4b43c189a2c6b4daaeeb0bcd7baba856cb286a32
Jens Petersen <juhpetersen@gmail.com>
2022-10-28 19:34:16 +0800

    tests: check if dnf-repo installed

--- test/tests.hs
+++ test/tests.hs
@@ -1,4 +1,6 @@
+import Data.Maybe (isJust)
 import SimpleCmd
+import System.Directory (findExecutable)
 import System.IO
 
 program :: [String] -> IO ()
@@ -6,18 +8,23 @@ program test = do
   timeIO $ cmdLog "pkgtreediff" test
   putStrLn ""
 
-tests :: [[String]]
-tests =
+tests :: Bool -> [[String]]
+tests dnfrepo =
   [
     ["-r", "test/files/35", "test/files/36"]
-  -- FIXME until native repoquerying use dnf-repo
-  , ["--ignore-release",
+  ]
+  ++
+  [
+    -- FIXME until native repoquerying use dnf-repo
+    ["--ignore-release",
      "dnf-repo -q -d * -u http://download.fedoraproject.org/pub/fedora/linux/releases/35/Everything/x86_64/os/ repoquery --qf %{name}-%{version}-%{release}.%{arch}",
      "dnf-repo -q -d * -u http://download.fedoraproject.org/pub/fedora/linux/releases/36/Everything/x86_64/os/ repoquery --qf %{name}-%{version}-%{release}.%{arch}"]
-  ]
+  | dnfrepo]
 
 main :: IO ()
 main = do
   hSetBuffering stdout NoBuffering
-  mapM_ program tests
-  putStrLn $ "\n" ++ show (length tests) ++ " command tests run"
+  have_dnfrepo <- findExecutable "dnf-repo"
+  let activeTests = tests $ isJust have_dnfrepo
+  mapM_ program activeTests
+  putStrLn $ "\n" ++ show (length activeTests) ++ " command tests run"
